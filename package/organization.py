cba35866527c9fcc0f34347c2f018160032bccbb
T. Cameron Waller <tcameronwaller@gmail.com>
2022-01-27 11:45:00 -0600

    create indicator variables for all categories rather than dropping one

--- package/organization.py
+++ package/organization.py
@@ -1930,7 +1930,7 @@ def create_categorical_variable_indicators(
         prefix_sep=separator,
         dummy_na=False, # whether to create indicators for missing values
         columns=[column_values],
-        drop_first=True, # whether to create "k - 1" dummies, adequate
+        drop_first=False, # whether to create "k - 1" dummies, adequate
         dtype=numpy.uint8,
     )
     table_indicators.set_index(
